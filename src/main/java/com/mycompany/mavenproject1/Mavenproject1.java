01c39f7138899cd7718ccbbfc472cb8394243b9f
DrGrodus <ebermd00@gmail.com>
2022-08-04 16:35:35 -0500

    prueba de rama

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.mavenproject1;
 
 /**
@@ -13,5 +8,6 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // esta es la rama 2
     }
 }
